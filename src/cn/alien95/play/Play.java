64d3952cb15c23c6b2c576ff96dc603fafd83903
llxdaxia <18983679028@163.com>
2016-04-04 11:10:33 +0800

    study

--- src/cn/alien95/play/Play.java
+++ src/cn/alien95/play/Play.java
@@ -6,16 +6,7 @@ package cn.alien95.play;
 public class Play {
 
     public static void main(String[] args) {
-        String a = "123";
-        System.out.println(a.hashCode());
-        String b = "123";
-        System.out.println(a.hashCode());
-        Object object = new Object();
-        System.out.println(object.hashCode());
-        Object object1 = new Object();
-        System.out.println(object1.hashCode());
-        System.out.println(a == b);
-        System.out.println(object == object1);
+
     }
 
 
--- src/cn/alien95/thread/ThreadLock.java
+++ src/cn/alien95/thread/ThreadLock.java
@@ -1,7 +1,5 @@
 package cn.alien95.thread;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 /**
  * Created by llxal on 2015/11/25.
  */
@@ -9,11 +7,9 @@ public class ThreadLock {
 
     Object lock = new Object();
     int num = 0;
-    static ReentrantLock lock1 = new ReentrantLock();
 
     public static void main(String[] args) {
-        lock1.lock();
-        lock1.unlock();
+
         ThreadLock test = new ThreadLock();
 
         Runnable runnable = new Runnable() {
